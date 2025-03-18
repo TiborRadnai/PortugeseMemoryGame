3b1f8c54c1fbebf36c99feecd3f09dee2f847b68
TiborRadnai <tibor.radnai@icloud.com>
2025-03-18 13:55:28 +0100

    quizfolders

---
+++ quizFolder/basic3/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Hogy vagy?",
+        answer: "A. Como vai?",
+        options: [
+                "A. Como vai?",
+                "B. De onde você é?",
+                "C. Como se diz?",
+                "D. O que você disse?"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Éhes vagyok.",
+        answer: "D. Estou com fome",
+        options: [
+                "A. De nada",
+                "B. Boa tarde",
+                "C. Até amanhã",
+                "D. Estou com fome"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Hogy mondja?",
+        answer: "B. Como se diz?",
+        options: [
+                "A. De onde você é?",
+                "B. Como se diz?",
+                "C. Eu não sei?",
+                "D. Quantos anos você tem?"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Szomjas vagyok.",
+        answer: "B. Estou com sede",
+        options: [
+                "A.Estou com fome",
+                "B. Estou com sede",
+                "C. Até mais tarde",
+                "D. Até amanhã"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Ismételje meg kérem!",
+        answer: "D. Repita, por favor!",
+        options: [
+                "A. Até mais tarde!",
+                "B. Tenha um bom dia!",
+                "C. É um prazer!",
+                "D. Repita, por favor!"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Nem értem.",
+        answer: "A. Eu não entendi",
+        options: [
+                "A. Eu não entendi",
+                "B. Eu não sei",
+                "C. De nada",
+                "D. Com licença"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Fáradt vagyok.",
+        answer: "C. Estou cansado",
+        options: [
+                "A. De nada",
+                "B. Eu não entendi",
+                "C. Estou cansado",
+                "D. Eu não sei"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Mit mondtál?",
+        answer: "A. O que você disse?",
+        options: [
+                "A. O que você disse?",
+                "B. Como se diz?",
+                "C. De onde você é?",
+                "D. Como vai?"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Örülök, hogy találkoztunk!",
+        answer: "D. É um prazer!",
+        options: [
+                "A. Repita, por favor!",
+                "B. Muito obrigado!",
+                "C. Até logo!",
+                "D. É um prazer!"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Viszlát holnap!",
+        answer: "C. Até amanhã!",
+        options: [
+                "A. Bom dia!",
+                "B. Até mais tarde!",
+                "C. Até amanhã!",
+                "D. Boa noite!"
+        ]
+    }
+];
---
+++ quizFolder/days/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Hét",
+        answer: "A. Semana",
+        options: [
+                "A. Semana",
+                "B. Dia",
+                "C. Mês",
+                "D. Ano"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Hétfő",
+        answer: "C. Segunda-feira",
+        options: [
+                "A. Quinta-feira",
+                "B. Quarta-feira",
+                "C. Segunda-feira",
+                "D. Sexta-feira"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Péntek",
+        answer: "A. Sexta-feira",
+        options: [
+                "A. Sexta-feira",
+                "B. Domingo",
+                "C. Sábado",
+                "D. Terça-feira"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Csütörtök",
+        answer: "D. Quinta-feira",
+        options: [
+                "A. Sexta-feira",
+                "B. Quarta-feira",
+                "C. Terça-feira",
+                "D. Quinta-feira"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Vasárnap",
+        answer: "A. Domingo",
+        options: [
+                "A. Domingo",
+                "B. Sábado",
+                "C. Quinta-feira",
+                "D. Quarta-feira"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Szerda",
+        answer: "C. Quarta-feira",
+        options: [
+                "A. Sexta-feira",
+                "B. Segunda-feira",
+                "C. Quarta-feira",
+                "D. Quinta-feira"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Nap",
+        answer: "A. Dia",
+        options: [
+                "A. Dia",
+                "B. Semana",
+                "C. Mês",
+                "D. Ano"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Szombat",
+        answer: "B. Sábado",
+        options: [
+                "A. Semana",
+                "B. Sábado",
+                "C. Domingo",
+                "D. Sexta-feira"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Kedd",
+        answer: "D. Terça-feira",
+        options: [
+                "A. Quinta-feira",
+                "B. Quarta-feira",
+                "C. Segunda-feira",
+                "D. Terça-feira"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Hétvége",
+        answer: "D. Final de semana",
+        options: [
+                "A. Semana",
+                "B. Os dias da semana",
+                "C. Os meses do ano",
+                "D. Final de semana"
+        ]
+    },
+];
